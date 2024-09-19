3c2d2bf1f22eaa69cb51d2a851fe335459efe20c
phtnnz <git16@mj.m-j-s.net>
2024-09-19 15:08:26 +0200

    New test script running 7z.exe

---
+++ my-qprocess-7z.py
@@ -0,0 +1,120 @@
+import re
+import sys
+from icecream import ic
+ic.enable()
+
+from PyQt6.QtCore import QProcess
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPlainTextEdit,
+    QProgressBar,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+STATES = {
+    QProcess.ProcessState.NotRunning: "Not running",
+    QProcess.ProcessState.Starting: "Starting...",
+    QProcess.ProcessState.Running: "Running...",
+}
+
+progress_re = re.compile(r"Total complete: (\d+)%")
+
+
+def simple_percent_parser(output):
+    """
+    Matches lines using the progress_re regex,
+    returning a single integer for the % progress.
+    """
+    m = progress_re.search(output)
+    if m:
+        pc_complete = m.group(1)
+        return int(pc_complete)
+
+
+def extract_vars(output):
+    """
+    Extracts variables from lines, looking for lines
+    containing an equals, and splitting into key=value.
+    """
+    data = {}
+    for s in output.splitlines():
+        if "=" in s:
+            name, value = s.split("=")
+            data[name] = value
+    return data
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Hold process reference.
+        self.p = None
+
+        layout = QVBoxLayout()
+
+        self.text = QPlainTextEdit()
+        layout.addWidget(self.text)
+
+        self.progress = QProgressBar()
+        layout.addWidget(self.progress)
+
+        btn_run = QPushButton("Execute")
+        btn_run.clicked.connect(self.start)
+
+        layout.addWidget(btn_run)
+
+        w = QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)
+
+    def start(self):
+        if self.p is not None:
+            return
+
+        self.p = QProcess()
+        self.p.readyReadStandardOutput.connect(self.handle_stdout)
+        self.p.readyReadStandardError.connect(self.handle_stderr)
+        self.p.stateChanged.connect(self.handle_state)
+        self.p.finished.connect(self.cleanup)
+        # self.p.start("python", ["my-dummy-script.py"])
+
+        # Run 7z.exe
+        #TODO: buffering in 7z.exe, -bs?? switches
+        self.p.start("C:/Program Files/7-Zip/7z.exe",
+                     [ "a", "-t7z", "-r", "-spf", "tmp/test.7z", "testdata" ]
+                    )
+
+        self.progress.setValue(0)
+
+    def handle_stderr(self):
+        result = bytes(self.p.readAllStandardError()).decode("utf8")
+        ic("stderr")
+        ic(result)
+        progress = simple_percent_parser(result)
+        if progress:
+            self.progress.setValue(progress)
+
+    def handle_stdout(self):
+        result = bytes(self.p.readAllStandardOutput()).decode("utf8")
+        ic("stdout")
+        ic(result)
+        if result:
+            data = extract_vars(result)
+
+        self.text.appendPlainText(str(data))
+
+    def handle_state(self, state):
+        self.statusBar().showMessage(STATES[state])
+
+    def cleanup(self):
+        self.p = None
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
