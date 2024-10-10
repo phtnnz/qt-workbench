5d3f8b48a979c0b068eb5952733f16baef34cf76
phtnnz <git16@mj.m-j-s.net>
2024-10-10 16:51:34 +0200

    Module verbose adapted to PyQt6, new methods set_widget(), set_stdout()

--- qverbose.py
+++ qverbose.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 Martin Junius
+# Copyright 2023-2024 Martin Junius
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,10 +32,16 @@
 #               .set_errno(errno)       relevant only for error()
 # Version 1.0 / 2024-10-10
 #       PyQt6 version of verbose module
+#       New:
+#               .set_widget(w)          set PyQt6 QPlainTextEdit widget for logging
+#               .set_stdout(flag)       enable print() output
 
 import sys
 
 # The following libs must be installed with pip
+
+# PyQt6
+from PyQt6.QtWidgets import QPlainTextEdit
 
 
 
@@ -48,35 +54,52 @@ NAME    = "qverbose"
 
 class Verbose:
     progname = None             # global program name
+    stdout = False              # Use print()
+    pyqt = None                 # QPlainTextEdit widget to show messages
 
-    def __init__(self, flag=False, prefix=None, abort=False):
+
+    def __init__(self, flag : bool=False, prefix : str=None, abort : bool=False):
         self.enabled = flag
         self.prefix = prefix
         self.abort = abort
         self.errno = 1          # exit(1) for generic errors
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args):
         if not self.enabled:
             return
+        txt_list = []
         if Verbose.progname:
-            print(Verbose.progname + ": ", end="")
+            txt_list.append(Verbose.progname + ":")
         if self.prefix:
-            print(self.prefix + ": ", end="")
-        print(*args, **kwargs)
+            txt_list.append(self.prefix + ":")
+        txt_list.extend(args)
+        txt = " ".join(txt_list)
+
+        if Verbose.stdout:
+            print(txt)
+        if Verbose.pyqt:
+            Verbose.pyqt.appendPlainText(txt)
+
         if self.abort:
             self._exit()
 
-    def enable(self, flag=True):
+    def enable(self, flag : bool=True):
         self.enabled = flag
 
     def disable(self):
         self.enabled = False
 
-    def set_prog(self, name):
+    def set_prog(self, name: str):
         Verbose.progname = name
 
-    def set_errno(self, errno):
+    def set_errno(self, errno: int):
         self.errno = errno
+
+    def set_widget(self, widget: QPlainTextEdit):
+        Verbose.pyqt = widget
+
+    def set_stdout(self, flag: bool=True):
+        Verbose.stdout = flag
 
     def _exit(self):
         if Verbose.progname:
